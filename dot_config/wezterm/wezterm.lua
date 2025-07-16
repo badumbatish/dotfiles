2cdd14cb6cbf623ceb9d08d6a9b706825a00af0b
usainzg <unaihtc70@gmail.com>
2025-07-16 12:16:52 +0200

    Automatic update via ~/.local/bin/cadd
    [Added]    - dot_config/wezterm/wezterm.lua

---
+++ dot_config/wezterm/wezterm.lua
@@ -0,0 +1,11 @@
+local wezterm = require("wezterm")
+
+local config = {}
+
+config.font = wezterm.font("BerkeleyMono Nerd Font")
+-- config.color_scheme = "IR_Black"
+-- config.color_scheme = "Catppuccin Mocha"
+config.color_scheme = "ayu"
+config.font_size = 14.0
+
+return config
