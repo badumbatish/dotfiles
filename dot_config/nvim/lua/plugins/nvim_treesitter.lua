5594de26d8afdd84247e97126ede2c73f7de677f
badumbatish <tanghocle456@gmail.com>
2025-09-12 19:34:14 -0700

    Automatic update via ~/.local/bin/cadd
    [Modified] - dot_config/nvim/lua/plugins/nvim_treesitter.lua

--- dot_config/nvim/lua/plugins/nvim_treesitter.lua
+++ dot_config/nvim/lua/plugins/nvim_treesitter.lua
@@ -11,7 +11,7 @@ return {
 
       require 'nvim-treesitter.configs'.setup {
         -- A list of parser names, or "all" (the five listed parsers should always be installed)
-        ensure_installed = "all",
+        ensure_installed = {},
         sync_install = false,
         auto_install = true,
         highlight = { enable = true },
