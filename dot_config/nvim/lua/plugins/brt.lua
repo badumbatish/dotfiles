269daf59c87e33724c8d7f1b992c39187ea592ea
badumbatish <tanghocle456@gmail.com>
2025-10-26 12:11:34 -0700

    Automatic update via ~/.local/bin/cadd
    [Modified] - dot_config/nvim/brt_commands.db
    [Modified] - dot_config/nvim/lazy-lock.json
    [Modified] - dot_config/nvim/lua/plugins/brt.lua

--- dot_config/nvim/lua/plugins/brt.lua
+++ dot_config/nvim/lua/plugins/brt.lua
@@ -1,15 +1,24 @@
 return {
-	"badumbatish/brt.nvim",
-	-- -- Uncomment these two lines to contribute and develop
-	dir = "~/Developer/nvim_proj/brt.nvim",
-	dev = { true },
-        dependencies = {
-          "ibhagwan/fzf-lua",  -- add fzf-lua as a dependency
-          "kkharji/sqlite.lua"
-        },
-	-- @t
-	config = function()
-		local brt = require("brt")
-		brt.setup()
-	end
+  {
+    "badumbatish/brt.nvim",
+    -- -- Uncomment these two lines to contribute and develop
+    dir = "~/Developer/nvim_proj/brt.nvim",
+    dev = { true },
+    dependencies = {
+      "ibhagwan/fzf-lua", -- add fzf-lua as a dependency
+      "kkharji/sqlite.lua"
+    },
+    -- @t
+    config = function()
+      local brt = require("brt")
+      brt.setup()
+    end
+  },
+  {
+    'stevearc/quicker.nvim',
+    ft = "qf",
+    ---@module "quicker"
+    ---@type quicker.SetupOptions
+    opts = {},
+  }
 }
