6d4c4d6cea58a8a5580622e8c9a3d2e9f96c21de
badumbatish <tanghocle456@gmail.com>
2025-11-02 12:08:02 -0800

    Automatic update via ~/.local/bin/cadd
    [Modified] - dot_config/nvim/brt_commands.db
    [Modified] - dot_config/nvim/lazy-lock.json
    [Modified] - dot_config/nvim/lua/plugins/aesthetics.lua
    [Modified] - dot_config/nvim/lua/plugins/blink_cmp.lua
    [Modified] - private_dot_local/bin/executable_sesh

--- dot_config/nvim/lua/plugins/blink_cmp.lua
+++ dot_config/nvim/lua/plugins/blink_cmp.lua
@@ -25,7 +25,9 @@ return {
     -- C-k: Toggle signature help (if signature.enabled = true)
     --
     -- See :h blink-cmp-config-keymap for defining your own keymap
-    keymap = { preset = 'default' },
+    keymap = {
+      preset = 'default',
+    },
 
     appearance = {
       -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
@@ -34,7 +36,47 @@ return {
     },
 
     -- (Default) Only show the documentation popup when manually triggered
-    completion = { documentation = { auto_show = false } },
+    completion = {
+      documentation = { auto_show = false },
+
+      menu = {
+        draw = {
+          components = {
+            kind_icon = {
+              text = function(ctx)
+                local icon = ctx.kind_icon
+                if vim.tbl_contains({ "Path" }, ctx.source_name) then
+                  local dev_icon, _ = require("nvim-web-devicons").get_icon(ctx.label)
+                  if dev_icon then
+                    icon = dev_icon
+                  end
+                else
+                  icon = require("lspkind").symbolic(ctx.kind, {
+                    mode = "symbol_text",
+                  })
+                end
+
+                return icon .. ctx.icon_gap
+              end,
+
+              -- Optionally, use the highlight groups from nvim-web-devicons
+              -- You can also add the same function for `kind.highlight` if you want to
+              -- keep the highlight groups in sync with the icons.
+              highlight = function(ctx)
+                local hl = ctx.kind_hl
+                if vim.tbl_contains({ "Path" }, ctx.source_name) then
+                  local dev_icon, dev_hl = require("nvim-web-devicons").get_icon(ctx.label)
+                  if dev_icon then
+                    hl = dev_hl
+                  end
+                end
+                return hl
+              end,
+            }
+          }
+        }
+      }
+    },
 
     -- Default list of enabled providers defined so that you can extend it
     -- elsewhere in your config, without redefining it, due to `opts_extend`
@@ -56,5 +98,5 @@ return {
     },
   },
 
-    opts_extend = { "sources.default" }
+  opts_extend = { "sources.default" }
 }
